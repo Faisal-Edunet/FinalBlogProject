8b3ac45bea912c66dde260ae36d093666979c2c4
Faisal-Edunet <mfaisal@edunetfoundation.org>
2025-12-17 14:42:15 +0530

    created blog project

---
+++ blogproject/authorapp/urls.py
@@ -0,0 +1,9 @@
+
+app_name = 'authorapp'
+from django.urls import path
+
+
+urlpatterns = [
+    # Add your URL patterns here
+]
+
---
+++ blogproject/blogapp/urls.py
@@ -0,0 +1,9 @@
+
+app_name = 'blogapp'
+from django.urls import path
+
+
+urlpatterns = [
+    # Add your URL patterns here
+]
+
